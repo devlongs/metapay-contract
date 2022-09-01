712bcc33977c12da10cae92671095f9fd8908b71
Longs Pemun Gotar <developerlongs@gmail.com>
2022-09-01 23:57:08 +0000

    contract factory

--- contracts/Metapay.sol
+++ contracts/Metapay.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
 
 contract Campaign {
     struct Request {
@@ -14,31 +30,33 @@ contract Campaign {
 
     Request[] public requests;
     address public manager;
-    uint public minimumContribution;
+    uint public minimunContribution;
     mapping(address => bool) public approvers;
+    uint public approversCount;
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    function Campaign(uint minimum) public {
-        manager = msg.sender;
-        minimumContribution = minimum;
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
+        minimunContribution = minimum;
     }
 
     function contribute() public payable {
-        require(msg.value > minimumContribution);
+        require(msg.value > minimunContribution);
 
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(
-        string description,
+        string memory description,
         uint value,
         address recipient
     ) public restricted {
-        Request memory newRequest = Request({
+        Request memory newRequst = Request({
             description: description,
             value: value,
             recipient: recipient,
@@ -46,16 +64,25 @@ contract Campaign {
             approvalCount: 0
         });
 
-        requests.push(newRequest);
+        requests.push(newRequst);
     }
 
     function approveRequest(uint index) public {
         Request storage request = requests[index];
-
         require(approvers[msg.sender]);
         require(!request.approvals[msg.sender]);
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
+    }
+
+    function finalizedRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
